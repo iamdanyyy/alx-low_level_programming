2d88df2a22b0dc190bce9206ba6b9a63e178c3ac
iamdanyyy <manudany543@gmail.com>
2023-05-23 11:18:37 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char print[] = "_putchar";
+	printf("%s\n", print);
+
+	return (0);
+}
