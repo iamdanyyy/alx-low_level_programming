4a640501cad00ec70808c9032cdf592889358983
iamdanyyy <manudany543@gmail.com>
2023-05-26 02:08:29 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9, except 2 and 4, then a new line
+ */
+
+void print_most_numbers(void)
+{
+	char num[] = "01356789";
+
+	_putchar(num);
+	_putchar('\n');
+}
