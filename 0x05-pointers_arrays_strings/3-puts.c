ff45efd6647f12026cb1238d10a399ae81da9073
iamdanyyy <manudany543@gmail.com>
2023-05-29 12:43:59 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	_putchar("str");
+	_putchar('\n');
+}
