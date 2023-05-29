959aa096b805ccd582c8fbc53053b8573a954244
iamdanyyy <manudany543@gmail.com>
2023-05-29 11:52:25 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string as parameter
+ * Return - Length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(*s);
+
+	return (length);
+}
