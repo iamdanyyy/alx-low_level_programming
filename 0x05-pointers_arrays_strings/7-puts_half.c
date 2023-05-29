5e7e674131a3ce7bf0870db0f6646cb5da43c08d
iamdanyyy <manudany543@gmail.com>
2023-05-29 14:57:55 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,30 +1,29 @@
 #include "main.h"
 #include <string.h>
 
-/**
- * puts_half - prints last half of a string
- * @str: string as parameter
- */
+
 
 void puts_half(char *str)
 {
 	int length = strlen(str);
+	int half;
 
-	if (length % 2 = 0)
+	if (length % 2 == 0)
 	{
-		int half = (length / 2);
+		half = (length / 2);
 	}
 
 	else
 	{
-		int half = (((length - 1) / 2) + 1);
+		half = (((length - 1) / 2) + 1);
 	}
 
 	int j;
-	int k
 
 	for (j = half; j < length; j++)
 	{
 		_putchar(str[j]);
 	}
+
+	_putchar('\n');
 }
