288addbadb8c247c2b58e041f24e4d7ae0f66c79
iamdanyyy <manudany543@gmail.com>
2023-05-29 13:18:02 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	char rev[];
+
+	rev = strrev(s);
+
+	printf("%s\n", rev);
+}
