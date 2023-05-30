af071ef1740f56fbf770480283ee03f04e992419
iamdanyyy <manudany543@gmail.com>
2023-05-30 05:42:47 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ * main - prints the alphabet in lowercase, then in uppercase, then a new line
  * Return: Always 0
  */
 
 int main(void) {
 
-	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
-	char ALPHA[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int k;
+	int j;
 
-	putchar("%c", alpha);
-	putchar("%c\n", ALPHA);
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		putchar(k);
+	}
+
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
 
 	return (0);
 
