2357b46ae975c51d54954b0422a93ed3c8277cbd
iamdanyyy <manudany543@gmail.com>
2023-05-30 05:55:03 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,19 @@
  * Return: Always 0
  */
 
-int main(void) {
+int main(void)
+{
 
 	int k;
 
 	for (k = 'a'; k <= 'z'; k++)
 	{
-		if (k = 'q', 'e')
+		if (k == 'e')
+		{
+			continue;
+		}
+
+		if(k == 'q')
 		{
 			continue;
 		}
