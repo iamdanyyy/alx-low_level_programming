3d29adb98b66afcb364197cdaf69efcba7f6c08d
iamdanyyy <manudany543@gmail.com>
2023-06-07 08:18:11 -0700

    0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: String to be passed as parameter
+ * Return: Length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int k = strlen(s);
+
+	return k;
+}
