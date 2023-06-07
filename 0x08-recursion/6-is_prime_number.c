d338a797735e19ea2f7b711a353efc3c04a02fa1
iamdanyyy <manudany543@gmail.com>
2023-06-07 14:07:47 -0700

    0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Integer passed as parameter
+ * Return: 1 if prime, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if(n
