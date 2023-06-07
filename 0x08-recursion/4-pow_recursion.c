ed15ac32a726ed932e1d000b57a7339770af8123
iamdanyyy <manudany543@gmail.com>
2023-06-07 12:36:31 -0700

    0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <math.h>
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: first integer as parameter
@@ -9,10 +9,11 @@
 
 int _pow_recursion(int x, int y)
 {
-
+	int power;
 	if (y > 0)
 	{
-		return (x ^ y);
+		power = pow(x, y);
+		return (power);
 	}
 
 	else if (y < 0)
