0403072557c6ab5a6fc6f3dad91a0f23b0d4bd6c
iamdanyyy <manudany543@gmail.com>
2023-06-20 01:55:10 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,9 @@
+/**
+ * SUM - returns the sum of x and y
+ * @x: integer as first parameter
+ * @y: integer as second parameter
+ */
+
 #ifndef SUM
-#define SUM(x, y) (x + y)
+#define SUM(x, y) ((x) + (y))
 #endif
