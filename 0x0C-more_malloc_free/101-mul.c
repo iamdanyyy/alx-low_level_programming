8c4c0cd1318cbe5143453d889a6b91dfc5b68eb0
iamdanyyy <manudany543@gmail.com>
2023-06-27 13:01:15 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -87,7 +87,8 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
-	{		if (result[i])
+	{
+		if (result[i])
 			a = 1;
 		if (a)
 			_putchar(result[i] + '0');
